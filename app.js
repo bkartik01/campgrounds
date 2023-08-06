74d20ae4ca7cbf1f8a41e69e35193965551777f6
bkartik01 <103896578+bkartik01@users.noreply.github.com>
2023-08-06 23:09:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -115,7 +115,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/do81mxsco",
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
